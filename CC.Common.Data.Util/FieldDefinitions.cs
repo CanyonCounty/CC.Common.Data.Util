0015598adb1a336105e3f5172e8c4ed4e7742207
Ken Wilcox <wilcoxk@gmail.com>
2014-12-15 10:48:22 -0700

    FieldDefinitions is a list of FieldDefinition items

---
+++ CC.Common.Data.Util/FieldDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CC.Common.Data.Util
+{
+  public class FieldDefinitions : List<FieldDefinition>
+  {
+    public new void Add(FieldDefinition value)
+    {
+      if (base.Find(x => x.Name.ToUpper().Equals(value.Name.ToUpper())) == null)
+        base.Add(value);
+    }
+  }
+}
