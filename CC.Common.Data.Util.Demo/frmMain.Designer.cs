f76a86c74530bf8b0faaeb3fa80893bf24102251
Ken Wilcox <wilcoxk@gmail.com>
2014-12-15 11:09:59 -0700

    Added Demo example

--- CC.Common.Data.Util.Demo/frmMain.Designer.cs
+++ CC.Common.Data.Util.Demo/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CC.Common.Data.Util.Demo
 {
-  partial class frmMain
+partial class frmMain
   {
     /// <summary>
     /// Required designer variable.
@@ -28,12 +28,105 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.btnDefine = new System.Windows.Forms.Button();
+      this.btnGenSQL = new System.Windows.Forms.Button();
+      this.splitMain = new System.Windows.Forms.SplitContainer();
+      this.lstFields = new System.Windows.Forms.ListBox();
+      this.txtOutput = new System.Windows.Forms.TextBox();
+      ((System.ComponentModel.ISupportInitialize)(this.splitMain)).BeginInit();
+      this.splitMain.Panel1.SuspendLayout();
+      this.splitMain.Panel2.SuspendLayout();
+      this.splitMain.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // btnDefine
+      // 
+      this.btnDefine.Location = new System.Drawing.Point(12, 12);
+      this.btnDefine.Name = "btnDefine";
+      this.btnDefine.Size = new System.Drawing.Size(107, 23);
+      this.btnDefine.TabIndex = 0;
+      this.btnDefine.Text = "Define Fields";
+      this.btnDefine.UseVisualStyleBackColor = true;
+      this.btnDefine.Click += new System.EventHandler(this.btnDefine_Click);
+      // 
+      // btnGenSQL
+      // 
+      this.btnGenSQL.Enabled = false;
+      this.btnGenSQL.Location = new System.Drawing.Point(125, 12);
+      this.btnGenSQL.Name = "btnGenSQL";
+      this.btnGenSQL.Size = new System.Drawing.Size(107, 23);
+      this.btnGenSQL.TabIndex = 1;
+      this.btnGenSQL.Text = "Generate SQL";
+      this.btnGenSQL.UseVisualStyleBackColor = true;
+      this.btnGenSQL.Click += new System.EventHandler(this.btnGenSQL_Click);
+      // 
+      // splitMain
+      // 
+      this.splitMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitMain.Location = new System.Drawing.Point(12, 41);
+      this.splitMain.Name = "splitMain";
+      this.splitMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitMain.Panel1
+      // 
+      this.splitMain.Panel1.Controls.Add(this.lstFields);
+      // 
+      // splitMain.Panel2
+      // 
+      this.splitMain.Panel2.Controls.Add(this.txtOutput);
+      this.splitMain.Size = new System.Drawing.Size(632, 264);
+      this.splitMain.SplitterDistance = 132;
+      this.splitMain.TabIndex = 4;
+      // 
+      // lstFields
+      // 
+      this.lstFields.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.lstFields.FormattingEnabled = true;
+      this.lstFields.Location = new System.Drawing.Point(0, 0);
+      this.lstFields.Name = "lstFields";
+      this.lstFields.Size = new System.Drawing.Size(632, 132);
+      this.lstFields.TabIndex = 8;
+      // 
+      // txtOutput
+      // 
+      this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.txtOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtOutput.Location = new System.Drawing.Point(0, 0);
+      this.txtOutput.Multiline = true;
+      this.txtOutput.Name = "txtOutput";
+      this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.txtOutput.Size = new System.Drawing.Size(632, 128);
+      this.txtOutput.TabIndex = 10;
+      // 
+      // frmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(656, 317);
+      this.Controls.Add(this.splitMain);
+      this.Controls.Add(this.btnGenSQL);
+      this.Controls.Add(this.btnDefine);
+      this.Name = "frmMain";
+      this.Text = "CC.Common.Data.Util.Demo";
+      this.Load += new System.EventHandler(this.frmMain_Load);
+      this.splitMain.Panel1.ResumeLayout(false);
+      this.splitMain.Panel2.ResumeLayout(false);
+      this.splitMain.Panel2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitMain)).EndInit();
+      this.splitMain.ResumeLayout(false);
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button btnDefine;
+    private System.Windows.Forms.Button btnGenSQL;
+    private System.Windows.Forms.SplitContainer splitMain;
+    private System.Windows.Forms.ListBox lstFields;
+    private System.Windows.Forms.TextBox txtOutput;
   }
 }
 
--- CC.Common.Data.Util.Demo/frmMain.cs
+++ CC.Common.Data.Util.Demo/frmMain.cs
@@ -6,14 +6,55 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CC.Common.Data.Util;
+using CC.Common.Data.Util.Dialect;
 
 namespace CC.Common.Data.Util.Demo
 {
   public partial class frmMain : Form
   {
+    private FieldDefinitions _fields;
+
     public frmMain()
     {
       InitializeComponent();
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+      _fields = new FieldDefinitions();
+    }
+
+    private DataType CustomTypeDelegate(FieldDefinition field)
+    {
+      return DataType.Text;
+    }
+
+    private void btnDefine_Click(object sender, EventArgs e)
+    {
+      _fields.Add(new FieldDefinition("My_table_id", DataType.Integer, 0));
+      _fields.Add(new FieldDefinition("my_field", DataType.Custom, 50, primary: false));
+      _fields.Add(new FieldDefinition("another_field", DataType.Text, 150, true));
+      _fields.Add(new FieldDefinition("another_field", DataType.Memo, 150, true));
+      _fields.Add(new FieldDefinition("a_date_field", DataType.Date, 0));
+      _fields.Add(new FieldDefinition("a_date_field", DataType.Date, 0));
+      _fields.Add(new FieldDefinition("SIGNATURE OF VOTER", DataType.Text, 150, true));
+      //Not Been Issued either DL # or SSN Flag
+      _fields.Add(new FieldDefinition("Not Been Issued either DL # or SSN Flag", DataType.Text, 150, true));
+      btnGenSQL.Enabled = true;
+
+      lstFields.Items.Clear();
+      foreach (FieldDefinition field in _fields)
+      {
+        lstFields.Items.Add(field.ToString());
+      }
+    }
+
+    private void btnGenSQL_Click(object sender, EventArgs e)
+    {
+      TableBuilder builder = new TableBuilder("MyTable", _fields, new SQLServerDialect());
+      builder.SetCustomTypeDelegate(CustomTypeDelegate);
+      txtOutput.Text = builder.ToString();
     }
   }
 }
